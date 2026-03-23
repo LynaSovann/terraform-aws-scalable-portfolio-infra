06a4f5573f66548fe344049f4eaa42792c11acfb
LynaSovann <sovannlyna2004@gmail.com>
2026-03-23 11:00:48 +0700

    add when to scale

--- terraform/asg.tf
+++ terraform/asg.tf
@@ -38,3 +38,17 @@ resource "aws_autoscaling_group" "web" {
     propagate_at_launch = true
   }
 }
+
+resource "aws_autoscaling_policy" "cpu_tracking" {
+  name                   = "cpu-target-tracking"
+  autoscaling_group_name = aws_autoscaling_group.web.name
+  policy_type            = "TargetTrackingScaling"
+
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+    target_value     = 50.0
+    disable_scale_in = false
+  }
+}
